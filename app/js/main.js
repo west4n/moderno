6788d45882c67fd849ef64b5899da0fac97b909a
Kursky Ivan <iskurskiii@gmail.com>
2020-02-12 22:10:41 +0300

    added product-slider, product

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,18 @@
 $(function(){
 
+    $('.rate-star').rateYo({
+        rating: 5,
+        starWidth: "15px",
+        readOnly: true,
+    });
+
+    var mixer = mixitup('.products__inner-box');
+
+   $('.product-slider__inner').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        dots: true,
+        arrows: false
+   });
+
 });
