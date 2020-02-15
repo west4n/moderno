0e834e1b0c90599f0a6fdc4fdc0db001fd7270fb
Kursky Ivan <iskurskiii@gmail.com>
2020-02-16 00:08:32 +0300

    added new-items page

--- app/js/main.js
+++ app/js/main.js
@@ -6,13 +6,38 @@ $(function(){
         readOnly: true,
     });
 
-    var mixer = mixitup('.products__inner-box');
-
-   $('.product-slider__inner').slick({
+    $('.product-slider__inner').slick({
         slidesToShow: 4,
         slidesToScroll: 4,
         dots: true,
         arrows: false
    });
+
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        grid: false,
+        skin: "round",
+        prefix: "$"
+    });
+
+    $('.icon-th-list').on('click', function(){
+        $('.product__item').addClass('list');
+        $('.icon-th-list').addClass('active');
+        $('.icon-th-large').removeClass('active')
+    });
+    $('.icon-th-large').on('click', function(){
+        $('.product__item').removeClass('list');
+        $('.icon-th-large').addClass('active');
+        $('.icon-th-list').removeClass('active')
+    });
+
+    var mixer = mixitup('.products__inner-box');
+
+   
+
 
 });
