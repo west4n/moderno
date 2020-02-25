866195be908fbd99b2a713e24c24011549dcb845
Kursky Ivan <iskurskiii@gmail.com>
2020-02-25 11:53:25 +0300

    added page upload, withdrawals

--- app/js/main.js
+++ app/js/main.js
@@ -52,6 +52,8 @@ $(function(){
             return false;
     });
 
+    $('input[type="file"], select').styler();
+
 
     var mixer = mixitup('.products__inner-box');
 
--- gulpfile.js
+++ gulpfile.js
@@ -21,6 +21,8 @@ gulp.task('sass', function(){
 gulp.task('style', function(){
     return gulp.src([
         
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.css',
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.theme.css',
         'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css',
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
@@ -35,6 +37,7 @@ gulp.task('style', function(){
 
 gulp.task('script', function(){
     return gulp.src([
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
         'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
         'node_modules/rateyo/src/jquery.rateyo.js',
         'node_modules/slick-carousel/slick/slick.js',
